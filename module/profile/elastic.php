a4ec171a13091f38e30fb687e5618a471c5fda3e
Christian Blanquera <cblanquera@openovate.com>
2017-01-09 11:25:09 +0800

    updating lock

---
+++ module/profile/elastic.php
@@ -0,0 +1,91 @@
+<?php return array (
+  'profile' => 
+  array (
+    'profile_image' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_name' => 
+    array (
+      'type' => 'string',
+      'fields' => 
+      array (
+        'keyword' => 
+        array (
+          'type' => 'keyword',
+        ),
+      ),
+    ),
+    'profile_email' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_phone' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_slug' => 
+    array (
+      'type' => 'string',
+      'fields' => 
+      array (
+        'keyword' => 
+        array (
+          'type' => 'keyword',
+        ),
+      ),
+    ),
+    'profile_detail' => 
+    array (
+      'type' => 'text',
+      'fields' => 
+      array (
+        'keyword' => 
+        array (
+          'type' => 'keyword',
+        ),
+      ),
+    ),
+    'profile_job' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_gender' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_birth' => 
+    array (
+      'type' => 'date',
+      'format' => 'yyyy-MM-dd',
+    ),
+    'profile_website' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_facebook' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_linkedin' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_twitter' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_google' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_type' => 
+    array (
+      'type' => 'string',
+    ),
+    'profile_flag' => 
+    array (
+      'type' => 'integer',
+    ),
+  ),
+);
